30a29d840f62a7207994b97e05346981f841a12e
Eonist <30n1st@gmail.com>
2019-11-21 19:04:20 +0100

    Add ios xcodeproject

---
+++ MathKitExampleIOS/MathKitExampleIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathKitExampleIOS
+//
+//  Created by Eon on 21/11/2019.
+//  Copyright © 2019 FutureLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
