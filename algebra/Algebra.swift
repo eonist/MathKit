d5a069c2d31dded0d801a51f8816c7eada314c90
eonist <30n1st@gmail.com>
2021-03-22 21:48:09 +0100

    Clean up

--- algebra/Algebra.swift
+++ algebra/Algebra.swift
@@ -8,9 +8,24 @@ class Algebra{
 	 * PARAM: c the number
 	 * EXAMPLE: Algebra.quadratic(0.5, 6, -68))//7.114877048604001
 	 */
-	static func quadratic(_ a:CGFloat,_ b:CGFloat,_ c:CGFloat)->CGFloat {
-		let numerator:CGFloat = -b+sqrt(b*b-4*a*c)
-		let denominator:CGFloat = 2*a
-		return numerator/denominator/*fraction*/
+	static func quadratic(_ a: CGFloat, _ b: CGFloat, _ c: CGFloat) -> CGFloat {
+		let numerator: CGFloat = -b + sqrt(b * b - 4 * a * c)
+		let denominator: CGFloat = 2 * a
+		return numerator / denominator // fraction
 	}
+	/**
+    * Find exponent when you have base and result
+    * - Parameters:
+    *   - base: 2^3 = 8 (2 is the base)
+    *   - value: 2^3 = 8 (8 is the value or the result if you like)
+    * ## Examples:
+    * print(exponent(base: 2, result: 4)) // 2
+    * print(exponent(base: 2, result: 8)) // 3
+    * print(exponent(base: 2, result: 16)) // 4
+    * print(exponent(base: 2, result: 128)) // 7
+    * print(exponent(base: 2, result: 256)) // 8
+    */
+   static func exponent(base: CGFloat, value: CGFloat) -> CGFloat {
+      log(value) / log(base) // var i: Int = 1; while pow(base, CGFloat(i)) < value { i += 1 }; return i
+   }
 }
